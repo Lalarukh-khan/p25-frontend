d87bce8a8a187165e10d30bcc7e990bb66e53864
Lalarukh Khan <lalarukhkhan179@gmail.com>
2024-01-28 15:52:07 +0500

    dashboard changes

--- src/views/userpanel/Dashboard.jsx
+++ src/views/userpanel/Dashboard.jsx
@@ -3,110 +3,157 @@
 // import axiosClient from "../../axios-client";
 // import $ from 'jquery';
 import 'react-daterange-picker/dist/css/react-calendar.css';
-import { Link } from "react-router-dom";
-import { Card, Col, Form, Row, Stack, Table } from "react-bootstrap";
+// import { Link } from "react-router-dom";
+import { Card, Col, Row, Table } from "react-bootstrap";
 // import axiosClient from '../../../axios-client';
 
 export default function UserDashboard() {
-  const MATERIAL_REQUISITION_STATUSES = [
-    {
-      title: "MR Created",
-      value: 100,
-      variant: 'primary'
-    },
-    {
-      title: "MR Approved",
-      value: 90,
-      variant: 'success'
-    },
-    {
-      title: "MR Pending",
-      value: 10,
-      variant: 'danger'
-    }
-  ]
+  // const MATERIAL_REQUISITION_STATUSES = [
+  //   {
+  //     title: "MR Created",
+  //     value: 100,
+  //     variant: 'primary'
+  //   },
+  //   {
+  //     title: "MR Approved",
+  //     value: 90,
+  //     variant: 'success'
+  //   },
+  //   {
+  //     title: "MR Pending",
+  //     value: 10,
+  //     variant: 'danger'
+  //   }
+  // ]
   
-  const MATERIAL_DELIVERY_STATUSES = [
-    {
-      title: 'Work in Progress',
-      value: 90
-    },
-    {
-      title: 'Delivery to site ',
-      value: 50
-    },
-    {
-      title: 'Delivery Pending',
-      value: 40
-    },
-    {
-      title: 'Installation done',
-      value: 10
-    }
-  ]
+  // const MATERIAL_DELIVERY_STATUSES = [
+  //   {
+  //     title: 'Work in Progress',
+  //     value: 90
+  //   },
+  //   {
+  //     title: 'Delivery to site ',
+  //     value: 50
+  //   },
+  //   {
+  //     title: 'Delivery Pending',
+  //     value: 40
+  //   },
+  //   {
+  //     title: 'Installation done',
+  //     value: 10
+  //   }
+  // ]
     return (
         <div>
           <Row>
         <Col>
           <Table bordered>
             <thead>
-              <tr>
-                <th>Category</th>
+              <tr style={{borderTop: "1px solid transparent"}}>
+                <th>Category &nbsp; &nbsp;  &nbsp; &nbsp;  &nbsp; &nbsp;  &nbsp; &nbsp;  &nbsp; &nbsp;  &nbsp; &nbsp;  &nbsp; &nbsp;  &nbsp; &nbsp;  &nbsp; &nbsp;  &nbsp; &nbsp;  &nbsp; &nbsp;  &nbsp; &nbsp;  &nbsp; &nbsp;  &nbsp; &nbsp; </th>
                 <th>Purchased Quantity</th>
                 <th>Warehouse Quantity</th>
                 <th>On site Quantity</th>
               </tr>
             </thead>
             <tbody>
-              <tr>
+              <tr style={{border: "1px solid transparent"}}>
                 <td colSpan={4}>Core System</td>
               </tr>
-              <tr>
-                <td><Link>Main Equipment</Link></td>
-                <td>5000</td>
-                <td>5000</td>
-                <td>5000</td>
+              <tr style={{borderBottom: "1px solid transparent"}}>
+                <td className="dashsub">Main Equipment &nbsp; &nbsp;  &nbsp;  &nbsp; &nbsp; &gt;</td>
+                <td style={{color: "black", fontWeight: "bold", fontSize: "18px"}}>100</td>
+                <td style={{color: "black", fontWeight: "bold", fontSize: "18px"}}>50</td>
+                <td style={{color: "black", fontWeight: "bold", fontSize: "18px"}}>10</td>
+              </tr>
+              <tr style={{border: "1px solid transparent"}}>
+                <td colSpan={4}>Core System 2</td>
+              </tr>
+              <tr style={{borderBottom: "1px solid transparent"}}>
+                <td className="dashsub">Sub Categrory 2&nbsp; &nbsp;  &nbsp;  &nbsp; &nbsp; &gt;</td>
+                <td style={{color: "black", fontWeight: "bold", fontSize: "18px"}}>5000</td>
+                <td style={{color: "black", fontWeight: "bold", fontSize: "18px"}}>5000</td>
+                <td style={{color: "black", fontWeight: "bold", fontSize: "18px"}}>5000</td>
+              </tr>
+              <tr style={{border: "1px solid transparent"}}>
+                <td colSpan={4}>Core System 3</td>
+              </tr>
+              <tr style={{borderBottom: "1px solid transparent"}}>
+                <td className="dashsub">Sub Categrory 3&nbsp; &nbsp;  &nbsp;  &nbsp; &nbsp; &gt;</td>
+                <td style={{color: "black", fontWeight: "bold", fontSize: "18px"}}>5000</td>
+                <td style={{color: "black", fontWeight: "bold", fontSize: "18px"}}>5000</td>
+                <td style={{color: "black", fontWeight: "bold", fontSize: "18px"}}>5000</td>
+              </tr>
+              <tr style={{border: "1px solid transparent"}}>
+                <td colSpan={4}>Core System 4</td>
+              </tr>
+              <tr style={{borderBottom: "1px solid transparent"}}>
+                <td className="dashsub">Sub Categrory 4&nbsp; &nbsp;  &nbsp;  &nbsp; &nbsp; &gt;</td>
+                <td style={{color: "black", fontWeight: "bold", fontSize: "18px"}}>5000</td>
+                <td style={{color: "black", fontWeight: "bold", fontSize: "18px"}}>5000</td>
+                <td style={{color: "black", fontWeight: "bold", fontSize: "18px"}}>5000</td>
+              </tr>
+              <tr style={{border: "1px solid transparent"}}>
+                <td colSpan={4}>Core System 5</td>
+              </tr>
+              <tr style={{borderBottom: "1px solid transparent"}}>
+                <td className="dashsub">Sub Categrory 5&nbsp; &nbsp;  &nbsp;  &nbsp; &nbsp; &gt;</td>
+                <td style={{color: "black", fontWeight: "bold", fontSize: "18px"}}>5000</td>
+                <td style={{color: "black", fontWeight: "bold", fontSize: "18px"}}>5000</td>
+                <td style={{color: "black", fontWeight: "bold", fontSize: "18px"}}>5000</td>
+              </tr>
+              <tr style={{border: "1px solid transparent"}}>
+                <td colSpan={4}>Core System 6</td>
+              </tr>
+              <tr style={{borderBottom: "1px solid transparent"}}>
+                <td className="dashsub">Sub Categrory 6&nbsp; &nbsp;  &nbsp;  &nbsp; &nbsp; &gt;</td>
+                <td style={{color: "black", fontWeight: "bold", fontSize: "18px"}}>5000</td>
+                <td style={{color: "black", fontWeight: "bold", fontSize: "18px"}}>5000</td>
+                <td style={{color: "black", fontWeight: "bold", fontSize: "18px"}}>5000</td>
               </tr>
             </tbody>
           </Table>
         </Col>
         <Col>
-          <Card>
+          <Card style={{background:"#F7F7F7"}}>
             <Card.Body>
-              <Card.Title className="mb-4">Material Requisition Status</Card.Title>
+              <Card.Title className="mb-4" style={{color: "black", textAlign: "center", fontWeight: "bold", fontSize: "25px"}}>Material Requisition Status</Card.Title>
 
-              <Stack direction="horizontal" gap={3} className="mb-5">
-                {MATERIAL_REQUISITION_STATUSES.map((item, idx) => (
-                  <Card
-                    key={idx}
-                    bg={item.variant}
-                    className="w-100"
-                    text="white"
-                  >
-                    <Card.Header>{item.title}</Card.Header>
-                    <Card.Body>
-                      <Card.Title style={{ fontSize: '2rem', color: '#fff'}}>{item.value}</Card.Title>
-                    </Card.Body>
-                  </Card>
-                ))}
-              </Stack>
+              <div className="row mb-5">
+                <div className="col-lg-4 col-md-4">
+                  <div style={{color: "white",fontSize:"30px", fontWeight:"bold",textAlign:"center",padding: "10px 0px", backgroundColor:"#F26422"}}>100</div>
+                  <h5 className="h5heading" style={{textAlign: "center"}}>MR Created</h5>
+                </div>
+                <div className="col-lg-4 col-md-4">
+                  <div style={{color: "white",fontSize:"30px", fontWeight:"bold",textAlign:"center",padding: "10px 0px", backgroundColor:"#3F9B49"}}>90</div>
+                  <h5 className="h5heading" style={{textAlign: "center"}}>MR Created</h5>
+                </div>
+                <div className="col-lg-4 col-md-4">
+                  <div style={{color: "white",fontSize:"30px", fontWeight:"bold",textAlign:"center",padding: "10px 0px", backgroundColor:"#C60000"}}>10</div>
+                  <h5 className="h5heading" style={{textAlign: "center"}}>MR Created</h5>
+                </div>
+              </div>
+              <Card.Title className="mb-4" style={{color: "black", textAlign: "center", fontWeight: "bold", fontSize: "25px"}}>Material Delivery Status</Card.Title>
 
-              <Card.Title className="mb-4">Material Delivery Status</Card.Title>
-
-              <Stack>
-                <Form>
-                  {MATERIAL_DELIVERY_STATUSES.map((item, idx) => (
-                    <Form.Group key={idx} as={Row} className="mb-3">
-                      <Form.Label column sm="3">
-                        {item.title}
-                      </Form.Label>
-                      <Col sm="9">
-                        <Form.Control disabled type="text" placeholder={item.title} value={item.value} />
-                      </Col>
-                    </Form.Group>
-                  ))}
-                </Form>
-              </Stack>
+              <div className="row">
+                <div className="col-lg-12">
+                  <input className="categinput mb-1" type="text" value="90" style={{fontSize: "30px", fontWeight: "bold"}}/>
+                  <h5 className="h5heading">Work in progress</h5>
+                </div>
+                <div className="col-lg-12">
+                  <input className="categinput mb-1" type="text" value="50" style={{fontSize: "30px", fontWeight: "bold"}}/>
+                  <h5 className="h5heading">Delivery to site</h5>
+                </div>
+                <div className="col-lg-12">
+                  <input className="categinput mb-1" type="text" value="40" style={{fontSize: "30px", fontWeight: "bold"}}/>
+                  <h5 className="h5heading">Delivery Pending</h5>
+                </div>
+                <div className="col-lg-12">
+                  <input className="categinput mb-1" type="text" value="10" style={{fontSize: "30px", fontWeight: "bold"}}/>
+                  <h5 className="h5heading">Installation Done</h5>
+                </div>
+              </div>
             </Card.Body>
           </Card>
         </Col>
